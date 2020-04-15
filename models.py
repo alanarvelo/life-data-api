d4d7cfa70269fb7e0658f932f279041dea8b0b32
Alan Arvelo <alanarvelo@gmail.com>
2020-04-15 18:18:00 -0400

    api advancements

--- models.py
+++ models.py
@@ -1,20 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from app import app
 
-app.config.from_object('../config')
+
+app.config.from_object('config')
 db = SQLAlchemy(app)
 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
 
-###########################  BOOKS  ######################
+############################  BOOKS  ###########################
 class Book(db.Model):
   __tablename__ = 'books'
   isbn = db.Column(db.Integer, primary_key=True, autoincrement=False)
   title = db.Column(db.String(500), nullable=False)
   author = db.Column(db.String(200), nullable=False)
   year_published = db.Column(db.DATE, nullable=False)
-  month_read = db.Column(db.DATE, default=now())
+  month_read = db.Column(db.DATE, nullable=False, default=datetime.today())
 
   def __repr__(self):
       return f'<Book — Title: {self.title}, Author: {self.author}, ISBN: {self.isbn}>'
