fab6a4e6761165f4e9f87b694ba1586ecccd5965
Alan Arvelo <alanarvelo@gmail.com>
2020-04-15 17:48:14 -0400

    initial commit

---
+++ api/models.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+
+app.config.from_object('../config')
+db = SQLAlchemy(app)
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+
+###########################  BOOKS  ######################
+class Book(db.Model):
+  __tablename__ = 'books'
+  isbn = db.Column(db.Integer, primary_key=True, autoincrement=False)
+  title = db.Column(db.String(500), nullable=False)
+  author = db.Column(db.String(200), nullable=False)
+  year_published = db.Column(db.DATE, nullable=False)
+  month_read = db.Column(db.DATE, default=now())
+
+  def __repr__(self):
+      return f'<Book — Title: {self.title}, Author: {self.author}, ISBN: {self.isbn}>'
