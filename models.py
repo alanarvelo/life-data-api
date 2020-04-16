78456c3f6fa29b4418f02fe708ab39480bbb312d
Alan Arvelo <alanarvelo@gmail.com>
2020-04-15 21:23:44 -0400

    modified books model

--- models.py
+++ models.py
@@ -10,14 +10,14 @@ db = SQLAlchemy(app)
 # Models.
 #----------------------------------------------------------------------------#
 
-############################  BOOKS  ###########################
+############################  BOOKS  ########################### https://www.goodreads.com/book/isbn/
 class Book(db.Model):
   __tablename__ = 'books'
   isbn = db.Column(db.String(50), primary_key=True, autoincrement=False)
   title = db.Column(db.String(500), nullable=False)
   author = db.Column(db.String(200), nullable=False)
-  year_published = db.Column(db.DATE, nullable=False)
-  month_read = db.Column(db.DATE, nullable=False, default=datetime.today())
+  year_published = db.Column(db.String(4), nullable=False)
+  date_read = db.Column(db.DATE, nullable=False, default=datetime.today())
 
   def __repr__(self):
-      return f'<Book — Title: {self.title}, Author: {self.author}, ISBN: {self.isbn}>'
+      return f'<Book — Title: {self.title}, Authors: {self.author}, ISBN: {self.isbn}>'
